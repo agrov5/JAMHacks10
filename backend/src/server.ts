ba0bc297facdb1390af2f2b375f7daebd37b1f56
qkanji <qkanji@icloud.com>
2026-06-13 13:50:05 -0400

    Some changes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -10,7 +10,7 @@ const port = Number(process.env.PORT ?? 3000);
 mongoose
   .connect(process.env.MONGO_URL || '')
   .then(() => console.log('Connected to MongoDB'))
-  .catch((err) => console.error('MongoDB connection error:', err));
+  .catch((err: any) => console.error('MongoDB connection error:', err));
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
