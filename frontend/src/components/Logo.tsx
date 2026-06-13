db09f0c1af6c4cbf81184cbdf15f88b6f697ba3a
qkanji <qkanji@icloud.com>
2026-06-13 17:43:26 -0400

    Favicon, title, and Cipher to CipherAI

--- frontend/src/components/Logo.tsx
+++ frontend/src/components/Logo.tsx
@@ -2,7 +2,7 @@ export default function Logo() {
   return (
     <span className="logo">
       <img src="/Cipher_AI.png" alt="CipherAI" style={{ height: 28, width: 'auto' }} draggable={false} />
-      Cipher
+      CipherAI
     </span>
   );
 }
